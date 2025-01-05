d5bdb418f02408c98e52d499a8550fd0c5adff36
aymkh <aymen4real@gmail.com>
2025-01-05 13:08:54 +0100

    chore: static assets - fonts - reomved unused images - added lucid-react for consistent icon sets

---
+++ src/app/lib/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lato } from "next/font/google";
+
+export const inter = Inter({
+    subsets: ["latin"],
+    weight: ["400", "700", "800"],
+});
+export const lato = Lato({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+});
