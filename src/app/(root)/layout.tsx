f0ca4b00dbf1f99d4e492676a5ef8be0bb3c08f8
aymkh <aymen4real@gmail.com>
2025-01-05 13:32:12 +0100

    chore: boilerplate clean up - changed folder structure - dynaic favicon based on theme

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { inter } from "./lib/fonts";
+import "../globals.css";
+import { inter } from "../../lib/fonts";
 
 export const metadata: Metadata = {
   title: "Soar | Financial Freedom",
   description: "Your all-in-one financial management platform.",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/soar_logo_light.png',
+      },
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/soar_logo.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
