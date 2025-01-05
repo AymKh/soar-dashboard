ee3de2bce9440cb184a312510ac9c8a2be83a946
aymkh <aymen4real@gmail.com>
2025-01-05 21:25:20 +0100

    feat! Added Sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://809b8736c79aa39675420290f40c350d@o4508592898179072.ingest.de.sentry.io/4508592905191504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/app/sentry-example-page/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { inter } from "../../lib/fonts";
+import TopNav from "@/components/topnav";
+import SideNav from "@/components/sidenav";
+
+export const metadata: Metadata = {
+    title: "Soar | Financial Freedom",
+    description: "Your all-in-one financial management platform.",
+    icons: {
+        icon: [
+            {
+                media: '(prefers-color-scheme: dark)',
+                url: '/soar_logo_light.png',
+            },
+            {
+                media: '(prefers-color-scheme: light)',
+                url: '/soar_logo.png',
+            },
+        ],
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} antialiased bg-secondary-100`}
+            >
+                <main className="lg:flex lg:flex-row justify-start">
+
+                    <SideNav />
+
+                    <section className="flex flex-col w-full">
+                        <TopNav />
+                        {children}
+                    </section>
+
+                </main>
+            </body>
+        </html>
+    );
+}
